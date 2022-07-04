fe072dcf342e53b1d9be6275be8304d33f2b4985
a-mizera <a.mizera.465@studms.ug.edu.pl>
2022-07-04 13:30:17 +0200

    pdf skonczony

--- zad1/src/views/OfferView.vue
+++ zad1/src/views/OfferView.vue
@@ -5,17 +5,19 @@
         <input type="number" v-model="ile"> <button @click="oblicz">Sprawdź</button>
         <p style="color:red;">{{errmessage}}</p>
         <div class="d-flex flex-row bg-light" >
-            <div class="d-flex flex-column mx-auto">
-                <div class="p-2">U nas zaplacisz</div>
-                <div class="p-2">{{value1}}</div></div>
+            
             <div class="d-flex flex-column mx-auto">
                 <div class="p-2">Na allegro zaplacisz</div>
                 <div class="p-2">{{value2}}</div>
                 </div>
+            <div class="d-flex flex-column mx-auto">
+                <div class="p-2">U nas zaplacisz</div>
+                <div class="p-2">{{value1}}</div></div>
             
-
         </div>
+        <p>Rocznie zaoszczędzisz {{oszczedz()}} </p>
     </div>
+    
 
 </template>
 <script>
@@ -25,7 +27,6 @@ export default{
     data(){
         return{
             errmessage:' ',
-            itemssold:50,
             value1:0,
             value2:0
 
@@ -40,11 +41,17 @@ export default{
                 this.value1=this.ile*0.25;
                 this.value2=this.ile*0.50;
                 this.errmessage=' ';
+                
                 }
             else{
                 this.errmessage='Podano błędną ilość!';
+                
+            }},
+            oszczedz(){
+                return this.value2*12-this.value1*12;
             }
-        },
+
+        
 
     }}
     
